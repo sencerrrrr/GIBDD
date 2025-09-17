0156118ae83bf242b227d1848d02d8d018025920
sencer <vakhitovdaniyar@mail.ru>
2025-09-17 16:20:01 +0300

    Создал две модели, одну не доделал

--- GIBDD/web/models.py
+++ GIBDD/web/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    name = models.CharField(
+        max_length=30,
+        help_text='Введите название роли',
+        verbose_name='Название роли',
+    )
+    description = models.CharField(
+        max_length=100,
+        help_text='Введите описание роли',
+        verbose_name='Описание роли',
+    )
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    firstname = models.CharField(
+        max_length=100,
+        help_text='Введите фамилию',
+        verbose_name='Фамилия',
+    )
+    name = models.CharField(
+        max_length=100,
+        help_text='Введите имя',
+        verbose_name='Имя',
+    )
+    fathername = models.CharField(
+        max_length=100,
+        help_text='Введите отчество',
+        verbose_name='Отчество',
+        null=True
+    )
