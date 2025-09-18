b28210d38f299fe5694cf2e26018cb842678e500
sencerrrrr <vakhitovdaniyar@mail.ru>
2025-09-18 11:02:38 +0300

    Создал таблицы регион и номер

--- GIBDD/web/admin.py
+++ GIBDD/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Role, User, Region, CarNumber
 
-# Register your models here.
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(Region)
+admin.site.register(CarNumber)
--- GIBDD/web/models.py
+++ GIBDD/web/models.py
@@ -1,4 +1,9 @@
+import random
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
 
 class Role(models.Model):
     name = models.CharField(
@@ -10,6 +15,8 @@ class Role(models.Model):
         max_length=100,
         help_text='Введите описание роли',
         verbose_name='Описание роли',
+        blank=True,
+        null=True,
     )
 
     def __str__(self):
@@ -30,5 +37,66 @@ class User(models.Model):
         max_length=100,
         help_text='Введите отчество',
         verbose_name='Отчество',
-        null=True
+        blank=True,
+        null=True,
     )
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return ' '.join(filter(None, [self.firstname, self.name, self.fathername]))
+
+
+VALID_LETTERS = 'АВЕКМНОРСТУХ'
+class Region(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text='Введите название региона',
+        verbose_name='Название региона',
+    )
+    number = models.CharField(
+        max_length=3,
+        help_text='Введите номер региона',
+        verbose_name='Номер региона',
+    )
+    def __str__(self):
+        return ' '.join(filter(None, [self.name, self.number]))
+
+class CarNumber(models.Model):
+    number = models.CharField(
+        max_length=10,
+        unique=True,
+        verbose_name='Номер автомобиля',
+    )
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.number
+
+    @staticmethod
+    def generate_number(region_number):
+        while True:
+            letter1 = random.choice(VALID_LETTERS)
+            digits = f'{random.randint(0, 999):03}'
+            letter2 = random.choice(VALID_LETTERS)
+            letter3 = random.choice(VALID_LETTERS)
+            number = f'{letter1}{digits}{letter2}{letter3}{region_number}'
+            if not CarNumber.objects.filter(number=number).exists():
+                return number
+
+    def save(self, *args, **kwargs):
+        if not self.number:
+            self.number = self.generate_number(self.region.number)
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        pattern = rf'^[{VALID_LETTERS}]\d{{3}}[{VALID_LETTERS}]{{2}}{self.region.number}$'
+        if not re.match(pattern, self.number):
+            raise ValidationError(f'Номер {self.number} не соответсвует формату для региона {self.region.number}')
+
+
